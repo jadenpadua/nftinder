972e88d1bbb7ea77e0f825b195748fcd74b918ce
Younus <yahmad@ucsd.edu>
2022-06-23 15:53:32 -0700

    Mock cards added

---
+++ constants/mockCards.ts
@@ -0,0 +1,15 @@
+export const mockCards = {
+    cards: [
+        {title: "NFT 1", id: "ID of NFT", image: "NFT 1.PNG", category: "Category of NFT", },
+        {title: "NFT 2", id: "ID of NFT", image: "NFT 2.PNG", category: "Category of NFT"},
+        {title: "NFT 3", id: "ID of NFT", image: "NFT 3.PNG", category: "Category of NFT"},
+        {title: "NFT 4", id: "ID of NFT", image: "NFT 4.PNG", category: "Category of NFT"},
+        {title: "NFT 5", id: "ID of NFT", image: "NFT 5.PNG", category: "Category of NFT"},
+        {title: "NFT 6", id: "ID of NFT", image: "NFT 6.PNG", category: "Category of NFT"},
+        {title: "NFT 7", id: "ID of NFT", image: "NFT 7.PNG", category: "Category of NFT"},
+        {title: "NFT 8", id: "ID of NFT", image: "NFT 8.PNG", category: "Category of NFT"},
+        {title: "NFT 9", id: "ID of NFT", image: "NFT 9.PNG", category: "Category of NFT"}
+    ]
+}
+
+
