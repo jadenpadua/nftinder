f98b84be8f561f1d2dbeed014591f61ae5a8cba7
Jaden Padua <jadenpadua@Jadens-MacBook-Pro.local>
2022-06-23 14:36:22 -0700

    Initial Boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./global.less";
+
+/**
+ * @description: Entrypoint of our application
+ */
+const App: React.FC = () => {
+  return <div className="app-container">NFTinder.io</div>;
+};
+
+export default App;
