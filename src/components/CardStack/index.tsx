591987bb87eff2997a94aed00a3045b37c4b6b5c
Younus <yahmad@ucsd.edu>
2022-06-24 15:24:50 -0700

    Finished card elements

--- src/components/CardStack/index.tsx
+++ src/components/CardStack/index.tsx
@@ -78,9 +78,47 @@ const CardStack = () => {
             }}
           >
             {/* Do flexbox stuff in here  */}
-            <Typography variant="h4" gutterBottom sx={{ fontWeight: "bold" }}>
-              {cards[i].title}
-            </Typography>
+            <div className="card-container">
+              <div className="title">
+                <Typography variant="h4" gutterBottom sx={{ fontWeight: "bold", pl: 1.5, pt: 4.25  }}>
+                  {cards[i].title}
+                </Typography>
+              </div>
+              <Typography variant="h5" gutterBottom sx={{ fontWeight: "bold", pl: 1.5 }}>
+                {cards[i].id}
+              </Typography>
+              <div className="footer">
+                <Typography variant="h4" gutterBottom sx={{ fontWeight: "bold", pl: 1, pt: 50 }}>
+                    Characteristics
+                </Typography>
+              </div>
+              <div className="bubbles-container">
+                <div className="bubble">
+                  <Typography variant="h4" gutterBottom sx={{ fontSize: 18, fontWeight: "bold" }}>
+                      Base
+                  </Typography>
+                  <Typography variant="h4" gutterBottom sx={{ fontSize: 18 }}>
+                      {cards[i].category}
+                  </Typography>
+                </div> 
+                <div className="bubble">
+                  <Typography variant="h4" gutterBottom sx={{ fontSize: 18, fontWeight: "bold" }}>
+                      Rarity
+                  </Typography>
+                  <Typography variant="h4" gutterBottom sx={{ fontSize: 18 }}>
+                      {cards[i].rarity}
+                  </Typography>
+                </div> 
+                <div className="bubble">
+                  <Typography variant="h4" gutterBottom sx={{ fontSize: 18, fontWeight: "bold" }}>
+                    Size
+                  </Typography>
+                  <Typography variant="h4" gutterBottom sx={{ fontSize: 18 }}>
+                    {cards[i].size}
+                  </Typography>
+                </div> 
+              </div>
+            </div>
           </animated.div>
         </animated.div>
       ))}
