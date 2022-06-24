db1fd86df83a662d19b5a6d8d2be491172d17d30
Jaden Padua <jadenpadua@Jadens-MacBook-Pro.local>
2022-06-23 18:14:39 -0700

    change pics to lowercase LOL

--- src/constants/mockCards.ts
+++ src/constants/mockCards.ts
@@ -17,7 +17,7 @@ export const mockCards: Cards = {
     {
       title: "CryptoGreen",
       id: "#5430",
-      image: require("../assets/CryptoGreen.png"),
+      image: require("../asset/CryptoGreen.png"),
       category: "Owl",
       rarity: "Common",
       size: "1:1",
@@ -26,7 +26,7 @@ export const mockCards: Cards = {
     {
       title: "CryptoDrip",
       id: "#5403",
-      image: require("../assets/CryptoDrip.png"),
+      image: require("../asset/CryptoDrip.png"),
       category: "Cat",
       rarity: "Rare",
       size: "1:1",
@@ -35,7 +35,7 @@ export const mockCards: Cards = {
     {
       title: "CryptoKitties",
       id: "#0039",
-      image: require(".././assets/CryptoKitties.png"),
+      image: require(".././asset/CryptoKitties.png"),
       category: "Cat",
       rarity: "Common",
       size: "1:1",
@@ -44,7 +44,7 @@ export const mockCards: Cards = {
     {
       title: "CryptoShock",
       id: "#0023",
-      image: require("../assets/CryptoShock.png"),
+      image: require("../asset/CryptoShock.png"),
       category: "Human",
       rarity: "Common",
       size: "1:1",
@@ -53,7 +53,7 @@ export const mockCards: Cards = {
     {
       title: "CryptoOwl",
       id: "#2134",
-      image: require("../assets/CryptoOwl.png"),
+      image: require("../asset/CryptoOwl.png"),
       category: "Owl",
       rarity: "Rare",
       size: "1:1",
@@ -62,7 +62,7 @@ export const mockCards: Cards = {
     {
       title: "CryptoPirate",
       id: "#4399",
-      image: require("../assets/CryptoPirate.png"),
+      image: require("../asset/CryptoPirate.png"),
       category: "Owl",
       rarity: "Common",
       size: "1:1",
@@ -71,7 +71,7 @@ export const mockCards: Cards = {
     {
       title: "CryptoBath",
       id: "#431403",
-      image: require("../assets/CryptoBath.png"),
+      image: require("../asset/CryptoBath.png"),
       category: "Ape",
       rarity: "Rare",
       size: "1:1",
@@ -80,7 +80,7 @@ export const mockCards: Cards = {
     {
       title: "ClassicApe",
       id: "#431404",
-      image: require("../assets/ClassicApe.png"),
+      image: require("../asset/ClassicApe.png"),
       category: "Ape",
       rarity: "Rare",
       size: "1:1",
@@ -89,7 +89,7 @@ export const mockCards: Cards = {
     {
       title: "BoredApe",
       id: "#431405",
-      image: require("../assets/BoredApe.png"),
+      image: require("../asset/BoredApe.png"),
       category: "Ape",
       rarity: "Common",
       size: "1:1",
