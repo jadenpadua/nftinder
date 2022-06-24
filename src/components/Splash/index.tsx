c1bdac7d8fd84f42c82646d7649ba9efd484e079
Gordon Chiu <gpchiu1213@gmail.com>
2022-06-24 16:05:45 -0700

    splash component

---
+++ src/components/Splash/index.tsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { Typography } from "@mui/material";
+import "./styles.less";
+const Splash: React.FunctionComponent = () => {
+    return (
+      <div className="splash-container">
+        
+      </div>
+    );
+  };
+  
+  export default Splash;
+  
