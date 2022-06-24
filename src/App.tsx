a3bc500b7d8b633d764c84f5d0c7c87b4c3e5fc4
Gordon Chiu <gpchiu1213@gmail.com>
2022-06-24 16:04:35 -0700

    Splash page wip

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import CardStack from "./components/CardStack";
 import Header from "./components/Header";
+import Splash from "./components/Splash";
 import "./global.less";
 
 /**
@@ -10,6 +11,8 @@ import "./global.less";
 const App = () => {
   return (
     <div className="app-container">
+      
+      <Splash/>
       <Header />
       <CardStack />
     </div>
