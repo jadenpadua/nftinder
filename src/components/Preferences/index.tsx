746996ef91677105c76254c10ff8c019604d2ebd
Jaden Padua <jadenpadua@Jadens-MacBook-Pro.local>
2022-06-25 12:49:48 -0700

    fix css

--- src/components/Preferences/index.tsx
+++ src/components/Preferences/index.tsx
@@ -3,19 +3,14 @@ import "./styles.less";
 import cards from "../../asset/cards.png";
 
 const Preferences: React.FunctionComponent = () => {
-    return (
-        <>
-        <div className="pref-container">
-            <div className="pref-card">
-
-            </div>
-
-            <div className="image-container">
-                <img src={cards} width="110%" height="110%" alt="" />
-            </div>
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="pref-container">
+        <div className="pref-card">Preferences</div>
+        <img src={cards} width="850" height="850" alt="" />
+      </div>
+    </>
+  );
+};
 
 export default Preferences;
