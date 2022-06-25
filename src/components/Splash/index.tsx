f6c8ed95b3cb22c71df1bbc01aeeb2092ec714d4
Jaden Padua <jadenpadua@Jadens-MacBook-Pro.local>
2022-06-25 11:47:33 -0700

    css styling changes

--- src/components/Splash/index.tsx
+++ src/components/Splash/index.tsx
@@ -1,45 +1,46 @@
 import React from "react";
 import "./styles.less";
-import {Button} from "@mui/material";
-import {Typography} from "@mui/material";
-import triangle from '../../asset/bg-triangle.png';
-import cards from '../../asset/cards.png';
+import { Button } from "@mui/material";
+import { Typography } from "@mui/material";
+import cards from "../../asset/cards.png";
 
 const Splash: React.FunctionComponent = () => {
-    return (
-      <div className="whole-splash">
-        <div className="splash-container">
-          <div className="flex-3">
-            <Typography
-              variant="h1"
-              gutterBottom
-              component="div"
-              sx={{ fontWeight: "bold",
-              fontFamily: "MonumentExtended-Regular", }}
-            >
-              Find Your Perfect NFT
-            </Typography>
-          </div>
-          <Button 
-              type="submit"
-              variant ="outlined"
-              style={{maxWidth: '13em', minWidth: '9em'}}
-              sx={ { 
-                  borderRadius: 5 ,
-                  border: "solid black 0.25em",
-                  fontSize: 30,
-                  color: "black",
-                  fontFamily: "MonumentExtended-Regular",} 
-              }
-              >Start Demo</Button>
-        </div>
-        <div className="background">
-          <img src={cards} width="100%" height="100%" alt=""/>
-        </div>
+  return (
+    <div className="whole-splash">
+      <div className="splash-container">
+        <Typography
+          variant="h1"
+          gutterBottom
+          component="div"
+          sx={{
+            fontWeight: "bold",
+            fontFamily: "MonumentExtended-Regular",
+            fontSize: 80,
+          }}
+        >
+          Find Your Perfect NFT
+        </Typography>
+
+        <Button
+          type="submit"
+          variant="outlined"
+          style={{ width: "13em", height: "3em", marginTop: "2%" }}
+          sx={{
+            borderRadius: 5,
+            border: "solid black 0.25em",
+            fontSize: 20,
+            color: "black",
+            fontFamily: "MonumentExtended-Regular",
+          }}
+        >
+          Start Demo
+        </Button>
       </div>
-        
-    );
-  };
-  
-  export default Splash;
-  
+      <div className="background">
+        <img src={cards} width="100%" height="100%" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Splash;
