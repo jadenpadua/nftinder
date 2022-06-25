8e70de9f9739fec893997dc57f2dc4700b6211ac
Gordon Chiu <gpchiu1213@gmail.com>
2022-06-24 17:11:39 -0700

    resolve conflicts

--- src/components/Splash/index.tsx
+++ src/components/Splash/index.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 import "./styles.less";
+import {Button} from "@mui/material";
+import {Typography} from "@mui/material";
 const Splash: React.FunctionComponent = () => {
-  return <div className="splash-container"></div>;
-};
-
-export default Splash;
+    return (
+      <div className="splash-container">
+        <div className="flex-3">
+          <Typography
+            variant="h1"
+            gutterBottom
+            component="div"
+            sx={{ fontWeight: "bold" }}
+          >
+            Find Your 
+          </Typography>
+        </div>
+        <div className="flex-3">
+          <Typography
+            variant="h1"
+            gutterBottom
+            component="div"
+            sx={{ fontWeight: "bold" }}
+          >
+            Perfect NFT
+          </Typography>
+        </div>
+        <div className="flex-3">
+            <Button 
+            type="submit"
+            color ="secondary"
+            variant ="outlined"
+            style={{maxWidth: '27em', maxHeight: '6em', minWidth: '27em', minHeight: '6em'}}
+            sx={ { borderRadius: 5 } }
+            >Start Demo</Button>
+        </div>
+        
+      </div>
+    );
+  };
+  
+  export default Splash;
+  
